203f3dd7a55eddefb5f45e0995989ce394722465
cagabi <cagabi@lapiluka.org>
2018-04-05 20:16:13 +0100

    Check user role to choose which mennu items to show

--- distribution_menu.php
+++ distribution_menu.php
@@ -1,7 +1,13 @@
 <?php
 
-    $domain = "messages";
-    bindtextdomain($domain, "Modules/schedule/locale");
-    bind_textdomain_codeset($domain, 'UTF-8');
+$domain = "messages";
+bindtextdomain($domain, "Modules/schedule/locale");
+bind_textdomain_codeset($domain, 'UTF-8');
 
-    $menu_dropdown[] = array('name'=> dgettext($domain, "Admin users and orgs"),'icon'=>'icon-user', 'path'=>"distribution/admin" , 'session'=>"write", 'order' => 1);
+global $mysqli, $user, $session;
+require_once "Modules/distribution/distribution_model.php";
+$distribution = new Distribution($mysqli, $user);
+
+$distro_user = $distribution->get_user($session['userid']);
+if ($distro_user['role'] == 'administrator')
+    $menu_dropdown[] = array('name' => dgettext($domain, "Admin users and orgs"), 'icon' => 'icon-user', 'path' => "distribution/admin", 'session' => "write", 'order' => 1);
