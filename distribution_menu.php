2427e9fedc4db1afc51b736544349d9869608733
cagabi <carlos@carbon.coop>
2018-05-24 22:31:35 +0100

    Removes icon from administration menu

--- distribution_menu.php
+++ distribution_menu.php
@@ -10,7 +10,7 @@ $distribution = new Distribution($mysqli, $user);
 
 $distro_user = $distribution->get_user($session['userid']);
 if ($distro_user['role'] == Roles::SUPERADMINISTRATOR || $distro_user['role'] == Roles::ADMINISTRATOR) {
-    $menu_dropdown[] = array('name' => dgettext($domain, "Administration"), 'icon' => 'icon-user', 'path' => "distribution/admin", 'session' => "write", 'order' => 1);
+    $menu_dropdown[] = array('name' => dgettext($domain, "Administration"), 'icon' => '', 'path' => "distribution/admin", 'session' => "write", 'order' => 1);
     $menu_dropdown[] = array('name' => dgettext($domain, "Day token"), 'icon' => '', 'path' => "distribution/daytoken", 'session' => "write", 'order' => 2);
     $menu_dropdown[] = array('name' => dgettext($domain, "Items"), 'icon' => '', 'path' => "distribution/items", 'session' => "write", 'order' => 3);
 }
