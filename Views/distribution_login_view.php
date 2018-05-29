8394fad7f42877c1299415435df3fc9a578ca4c8
cagabi <carlos@carbon.coop>
2018-05-29 09:23:46 +0100

    day token fixed: was redirecting to locahost

--- Views/distribution_login_view.php
+++ Views/distribution_login_view.php
@@ -22,7 +22,7 @@ bind_textdomain_codeset($domain2, 'UTF-8');
     $('#content').html(result_html);
 
     //result_html = '<div id"token-login"><p>Day token</p><p><input type="text" tabindex="2" name="token"></p></div>';
-    result_html = '<form id="login-form" method="post" action="http://localhost/utopia56DMS/distribution/tokenlogin">';
+    result_html = '<form id="login-form" method="post" action="distribution/tokenlogin">';
     result_html += '<label>Day token<input type="text" tabindex="2" name="day_token"></label>';
     result_html += '<button id="token-login" class="btn-primary" type="submit">Token login</button>';
     result_html += '</form>';
